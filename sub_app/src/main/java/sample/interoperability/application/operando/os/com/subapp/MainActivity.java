2f2108c16333fbd866e5c644140ed6f7902e780e
Shinobu Okano <okanoshinobu8@gmail.com>
2017-08-24 20:59:33 +0900

    first commit

---
+++ sub_app/src/main/java/sample/interoperability/application/operando/os/com/subapp/MainActivity.java
@@ -0,0 +1,13 @@
+package sample.interoperability.application.operando.os.com.subapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
